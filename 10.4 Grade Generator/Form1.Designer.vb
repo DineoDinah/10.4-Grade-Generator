5fcf35e0ca7cff35eda1b3071fb48cc4b8164077
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-05 00:27:56 +0200

    Add project files.

---
+++ 10.4 Grade Generator/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		BtnCompute = New Button()
+		Label1 = New Label()
+		TextBox1 = New TextBox()
+		Label2 = New Label()
+		SuspendLayout()
+		' 
+		' BtnCompute
+		' 
+		BtnCompute.Location = New Point(208, 231)
+		BtnCompute.Name = "BtnCompute"
+		BtnCompute.Size = New Size(190, 34)
+		BtnCompute.TabIndex = 0
+		BtnCompute.Text = "Compute Grade"
+		BtnCompute.UseVisualStyleBackColor = True
+		' 
+		' Label1
+		' 
+		Label1.AutoSize = True
+		Label1.Location = New Point(144, 125)
+		Label1.Name = "Label1"
+		Label1.Size = New Size(137, 25)
+		Label1.TabIndex = 1
+		Label1.Text = "Enter Your Mark"
+		' 
+		' TextBox1
+		' 
+		TextBox1.Location = New Point(303, 125)
+		TextBox1.Name = "TextBox1"
+		TextBox1.Size = New Size(150, 31)
+		TextBox1.TabIndex = 2
+		' 
+		' Label2
+		' 
+		Label2.AutoSize = True
+		Label2.Font = New Font("Showcard Gothic", 18F, FontStyle.Underline, GraphicsUnit.Point)
+		Label2.Location = New Point(272, 41)
+		Label2.Name = "Label2"
+		Label2.Size = New Size(350, 44)
+		Label2.TabIndex = 3
+		Label2.Text = "Grade Generator"
+		' 
+		' Form1
+		' 
+		AutoScaleDimensions = New SizeF(10F, 25F)
+		AutoScaleMode = AutoScaleMode.Font
+		BackColor = SystemColors.ControlDark
+		ClientSize = New Size(800, 450)
+		Controls.Add(Label2)
+		Controls.Add(TextBox1)
+		Controls.Add(Label1)
+		Controls.Add(BtnCompute)
+		Name = "Form1"
+		Text = "Form1"
+		ResumeLayout(False)
+		PerformLayout()
+	End Sub
+
+	Friend WithEvents BtnCompute As Button
+	Friend WithEvents Label1 As Label
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents Label2 As Label
+End Class
---
+++ 10.4 Grade Generator/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnCompute.Click
+
+
+		Dim Mark As Integer
+		Dim Grade As String
+		Mark = Val(TextBox1.Text)
+		If Mark >= 80 Then
+			Grade = "A"
+		ElseIf Mark >= 60 And Mark < 80 Then
+
+			Grade = "B"
+
+		ElseIf Mark >= 50 And Mark < 60 Then
+			Grade = "C"
+		Else
+			Grade = "D"
+		End If
+		MsgBox("You grade is " & Grade)
+
+	End Sub
+
+End Class
